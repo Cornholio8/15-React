c4e575c2a8a0911f7bb63109dbd3e72ad1ac29b6
Piotr Kamecki <piotrkamecki8@gmail.com>
2022-12-28 16:41:42 +0100

    add searching

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,6 +8,8 @@ const reducer = (state, action) => {
         return { ...state, columns: [...state.columns, { ...action.payload, id: shortid() }]};
       case 'ADD_CARD':
         return { ...state, cards: [...state.cards, { ...action.payload, id: shortid() }]};
+      case 'UPDATE_SEARCHSTRING':
+        return { ...state, searchString: action.searchString };
       default:
         return state;
     }
